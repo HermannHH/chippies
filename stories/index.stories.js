b8b3d0e7cb33b641e47585674d4888294cbe3767
Hermann Harris <harris.hermann@gmail.com>
2018-04-08 19:48:14 +0200

    Storiebook fully setup

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -6,11 +6,31 @@ import { linkTo } from '@storybook/addon-links';
 
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
 
+import { withInfo } from '@storybook/addon-info';
+
 import { Button, Welcome } from '@storybook/react/demo';
 
 const stories = storiesOf('Storybook Knobs', module);
 
 stories.addDecorator(withKnobs);
+
+stories.add('simple info',
+    withInfo({
+      styles: {
+        header: {
+          h1: {
+            color: 'red'
+          }
+        }
+      },
+      text: 'String or React Element with docs about my component', // Warning! This option's name will be likely renamed to "summary" in 3.3 release. Follow this PR #1501 for details
+      // other possible options see in Global options section below
+    })(() =>
+<button disabled={boolean('Disabled', false)} >
+    {text('Label', 'Hello Button')}
+  </button>
+    )
+  )
 
 // Knobs for React props
 stories.add('with a button', () => (
