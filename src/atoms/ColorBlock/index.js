58b7b9689c3926dcbba7949172696097f95b789a
Hermann Harris <harris.hermann@gmail.com>
2018-04-25 22:06:35 +0200

    Removed all windows gunk

---
+++ src/atoms/ColorBlock/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ColorBlock() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default ColorBlock;
---
+++ src/atoms/ColorBlock/index.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { boolean, text } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
+import atoms from '../index.stories';
+
+import ColorBlock from './index';
+
+atoms.add(
+  'ColorBlock',
+  withInfo(`
+      description or documentation about my component, supports markdown
+    
+      ~~~js
+      <ColorBlock />
+      ~~~
+    
+    `)(props => <ColorBlock />),
+);
