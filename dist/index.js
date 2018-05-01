47e4036c1cf8f95f00c8425ffc4fbdd97087c693
Hermann Harris <harris.hermann@gmail.com>
2018-05-01 14:31:01 +0200

    FFS 2

--- dist/index.js
+++ dist/index.js
@@ -3,47 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.ThemeWrapper = exports.Pad = exports.Paragraph = exports.Heading = exports.PaletteBlock = exports.ColorDescription = exports.ColorBlock = exports.Button = undefined;
+exports.Button = undefined;
 
 var _Button = require('./Button');
 
 var _Button2 = _interopRequireDefault(_Button);
 
-var _ColorBlock = require('./ColorBlock');
-
-var _ColorBlock2 = _interopRequireDefault(_ColorBlock);
-
-var _ColorDescription = require('./ColorDescription');
-
-var _ColorDescription2 = _interopRequireDefault(_ColorDescription);
-
-var _Heading = require('./Heading');
-
-var _Heading2 = _interopRequireDefault(_Heading);
-
-var _PaletteBlock = require('./PaletteBlock');
-
-var _PaletteBlock2 = _interopRequireDefault(_PaletteBlock);
-
-var _Paragraph = require('./Paragraph');
-
-var _Paragraph2 = _interopRequireDefault(_Paragraph);
-
-var _Pad = require('./Pad');
-
-var _Pad2 = _interopRequireDefault(_Pad);
-
-var _ThemeWrapper = require('./ThemeWrapper');
-
-var _ThemeWrapper2 = _interopRequireDefault(_ThemeWrapper);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.Button = _Button2.default;
-exports.ColorBlock = _ColorBlock2.default;
-exports.ColorDescription = _ColorDescription2.default;
-exports.PaletteBlock = _PaletteBlock2.default;
-exports.Heading = _Heading2.default;
-exports.Paragraph = _Paragraph2.default;
-exports.Pad = _Pad2.default;
-exports.ThemeWrapper = _ThemeWrapper2.default;
