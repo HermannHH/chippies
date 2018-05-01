8a35b615fdead80110663a5f04f29e64c8981891
Hermann Harris <harris.hermann@gmail.com>
2018-05-01 14:27:43 +0200

    FFS

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,3 @@
 import Button from './Button';
-import ColorBlock from './ColorBlock';
-import ColorDescription from './ColorDescription';
-import Heading from './Heading';
-import PaletteBlock from './PaletteBlock';
-import Paragraph from './Paragraph';
-import Pad from './Pad';
-import ThemeWrapper from './ThemeWrapper';
 
-export {
-  Button,
-  ColorBlock,
-  ColorDescription,
-  PaletteBlock,
-  Heading,
-  Paragraph,
-  Pad,
-  ThemeWrapper,
-};
+export { Button  };
