f84846126eefff9b51e688d3b68e44c3c537d654
Hermann Harris <harris.hermann@gmail.com>
2018-05-07 21:20:48 +0200

    Setup icon library

---
+++ src/components/Icon/fontawesome.js
@@ -0,0 +1,12 @@
+import {
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
+// import * as tt from '@fortawesome/fontawesome-svg-core';
+// import * as yy from '@fortawesome/free-regular-svg-icons';
+// import * as xx from '@fortawesome/free-solid-svg-icons';
+
+// console.log('tttt', tt, yy, xx)
+
+export default {
+  faPlus,
+};
---
+++ src/components/Icon/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Shevy from 'shevyjs';
+import kratedTheme from 'krated-theme';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import fa from './fontawesome';
+
+
+import { shevyConfig } from '../helpers';
+
+const shevy = new Shevy(shevyConfig);
+const {
+  content,
+} = shevy;
+
+
+function Icon({
+ theme, icon, color, shade, size,
+}) {
+  return (
+    <FontAwesomeIcon
+      icon={fa[icon]}
+      color={theme.colors[color][shade]}
+      size={size}
+    />
+  );
+}
+
+Icon.propTypes = {
+  theme: PropTypes.shape(),
+  icon: PropTypes.oneOf([
+    'faPlus',
+  ]).isRequired,
+  color: PropTypes.string,
+  shade: PropTypes.string,
+  size: PropTypes.oneOf(['lg', 'xs', 'sm', '1x', '2x', '3x', '4x', '5x', '6x', '7x', '8x', '9x', '10x']),
+};
+
+Icon.defaultProps = {
+  theme: kratedTheme,
+  color: 'grey',
+  shade: '0',
+  size: 'sm',
+};
+
+export default Icon;
