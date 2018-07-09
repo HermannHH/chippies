bb045e149702795d32ffcda54c796deb6960a6c3
Hermann Harris <harris.hermann@gmail.com>
2018-07-09 21:24:27 +0200

    Refactor import export module

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,135 @@
-const req = require.context('.', true, /\.\/[^/]+\/index\.js$/);
-req.keys().forEach((key) => {
-  const componentName = key.replace(/^.+\/([^/]+)\/index\.js/, '$1');
-  module.exports[componentName] = req(key).default;
-});
+import AcceptQuote from './AcceptQuote';
+import AppBar from './AppBar';
+import Border from './Border';
+import BrandedMixedLogo from './BrandedMixedLogo';
+import BrandedWhiteIcon from './BrandedWhiteIcon';
+import BrandHeader from './BrandHeader';
+import BrochureFooter from './BrochureFooter';
+import Button from './Button';
+import Card from './Card';
+import CardActions from './CardActions';
+import CardCover from './CardCover';
+import CardGrid from './CardGrid';
+import CardPad from './CardPad';
+import CardTitle from './CardTitle';
+import Checkbox from './Checkbox';
+import CodeSnippet from './CodeSnippet';
+import ColorBlock from './ColorBlock';
+import ColorDescription from './ColorDescription';
+import ColorPalette from './ColorPalette';
+import Display from './Display';
+import Divider from './Divider';
+import EscapeButton from './EscapeButton';
+import FlexPosition from './FlexPosition';
+import FormActionContainer from './FormActionContainer';
+import FormInputContainer from './FormInputContainer';
+import Heading from './Heading';
+import HeadingGroup from './HeadingGroup';
+import helpers from './helpers';
+import Hyperlink from './Hyperlink';
+import Icon from './Icon';
+import Image from './Image';
+import ImageBlock from './ImageBlock';
+import InputErrors from './InputErrors';
+import InspireMessageHeading from './InspireMessageHeading';
+import InteractiveLineItem from './InteractiveLineItem';
+import Label from './Label';
+import LineItems from './LineItems';
+import LoadingScreen from './LoadingScreen';
+import LonelyIsland from './LonelyIsland';
+import Modal from './Modal';
+import Mrg from './Mrg';
+import NumericSpin from './NumericSpin';
+import Pad from './Pad';
+import PaletteBlock from './PaletteBlock';
+import Paragraph from './Paragraph';
+import PlacesInput from './PlacesInput';
+import PoweredByKrated from './PoweredByKrated';
+import ProductCard from './ProductCard';
+import ProgressiveImage from './ProgressiveImage';
+import Pulse from './Pulse';
+import PulsingBrandedWhiteIcon from './PulsingBrandedWhiteIcon';
+import QuoteOnboard from './QuoteOnboard';
+import QuoteRequestForm from './QuoteRequestForm';
+import QuoteRequestModal from './QuoteRequestModal';
+import QuoteSlideBar from './QuoteSlideBar';
+import RejectQuote from './RejectQuote';
+import Scroller from './Scroller';
+import SlideBar from './SlideBar';
+import Small from './Small';
+import TextAlign from './TextAlign';
+import TextArea from './TextArea';
+import TextInput from './TextInput';
+import TextSection from './TextSection';
+import Toggle from './Toggle';
+import VerticalMiddle from './VerticalMiddle';
+import Well from './Well';
 
+export {
+  AcceptQuote,
+  AppBar,
+  Border,
+  BrandedMixedLogo,
+  BrandedWhiteIcon,
+  BrandHeader,
+  BrochureFooter,
+  Button,
+  Card,
+  CardActions,
+  CardCover,
+  CardGrid,
+  CardPad,
+  CardTitle,
+  Checkbox,
+  CodeSnippet,
+  ColorBlock,
+  ColorDescription,
+  ColorPalette,
+  Display,
+  Divider,
+  EscapeButton,
+  FlexPosition,
+  FormActionContainer,
+  FormInputContainer,
+  Heading,
+  HeadingGroup,
+  helpers,
+  Hyperlink,
+  Icon,
+  Image,
+  ImageBlock,
+  InputErrors,
+  InspireMessageHeading,
+  InteractiveLineItem,
+  Label,
+  LineItems,
+  LoadingScreen,
+  LonelyIsland,
+  Modal,
+  Mrg,
+  NumericSpin,
+  Pad,
+  PaletteBlock,
+  Paragraph,
+  PlacesInput,
+  PoweredByKrated,
+  ProductCard,
+  ProgressiveImage,
+  Pulse,
+  PulsingBrandedWhiteIcon,
+  QuoteOnboard,
+  QuoteRequestForm,
+  QuoteRequestModal,
+  QuoteSlideBar,
+  RejectQuote,
+  Scroller,
+  SlideBar,
+  Small,
+  TextAlign,
+  TextArea,
+  TextInput,
+  TextSection,
+  Toggle,
+  VerticalMiddle,
+  Well,
+};
